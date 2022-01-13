27024464f5a8a8f910800cb1d9d18f45ec112ad3
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2022-01-13 12:03:55 +0700

    clear

--- components/organisms/hero-carousel/index.js
+++ components/organisms/hero-carousel/index.js
@@ -21,11 +21,11 @@ const BannerCarousel = ({ nowPlaying }) => {
            <div className=''>
               <ItemCarouselHero key={movie.id} movie={movie} />
               <article className='block lg:hidden bg-gradient-mobile px-2 py-4 w-full'>
-                <p className='text-white text-xl font-semibold text-center'>{movie.original_title}</p>
-                
-                <p className='truncate text-ellipsis text-clip  font-light text-white'>{movie.overview}</p>
+                <p className='text-white md:text-4xl text-xl font-semibold text-center'>{movie.original_title}</p>
+                <Gap className='h-5 hidden md:block' />
+                <p className='truncate text-ellipsis text-clip  font-light text-white md:font-normal md:w-10/12 md:mx-auto '>{movie.overview}</p>
                 <Gap className='h-3' />
-                <button className='text-gray-100 rounded-md px-4 py-2 text-left bg-red-700'>watch now</button>
+                <button className='text-gray-100 rounded-md px-4 py-2 text-left bg-red-700 md:text-lg'>watch now</button>
                 <Gap className='h-3' />
               </article>
            </div>
