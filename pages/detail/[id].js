505e372bb9bcc830a1523bd55fabd88fae2d295b
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2022-01-15 12:12:45 +0700

    add image

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+import API_ENDPOINT from '../../utils/config/api-endpoint'
+import CONFIG from '../../utils/config/config'
 import { fetchDetail } from "../../utils/fetch-ssr"
 
 export async function getServerSideProps({params}) {
@@ -11,7 +14,9 @@ const Detail = ({reqDetail}) => {
   
   return (
     <div>
+      <Image src={CONFIG.BASE_IMAGE_URL + reqDetail.poster_path} width={200} height={300}/>
       <p>{reqDetail.title}</p>
+      <img src={CONFIG.BASE_IMAGE_URL + reqDetail.backdrop_path} alt="" />
     </div>
   )
 }
