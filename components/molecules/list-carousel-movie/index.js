adc95700879da621c2baccccbc4144e09a60097e
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2022-01-15 16:52:44 +0700

    berhasil react-carousel

--- components/molecules/list-carousel-movie/index.js
+++ components/molecules/list-carousel-movie/index.js
@@ -1,43 +1,55 @@
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
+
 import Image from 'next/image'
 import Slider from 'react-slick'
 import { MoviesThumbnail } from '../..'
 import CONFIG from '../../../utils/config/config'
 
+const PrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div
+      className='border block w-7 h-full cursor-pointer absolute right-0 z-10 top-0'
+      onClick={onClick}
+    >s</div>
+  );
+  
+}
+
+ 
+const NextArrow = (props) => {
+  const {onClick} = props
+  return (
+  <div
+      className='border block w-7 h-full cursor-pointer absolute left-0 z-10 top-0'
+      onClick={onClick}
+    >s</div>
+  )
+}
 
 const ListCarouselMovie = ({ movies }) => {
   const myMovie = movies
   const movieListFirst = myMovie.filter(el => movies.indexOf(el) < 10)
   const movieListSecond = movies.filter(el => movies.indexOf(el) > 9 )
 
+ 
+
   const sliderSettings = {
-    centerMode: true,
-			infinite: true,
-			centerPadding: "60px",
-			slidesToShow: 3,
-			speed: 500,
-			rows: 2,
-			slidesPerRow: 2
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />
   }
   return (
+    <div className='border'>
+    <h2> Single Item</h2>
+    
     <Slider {...sliderSettings}>
-      <div>
-        <p>1</p>
-      </div>
-      <div>
-        <p>1</p>
-      </div>
-      <div>
-        <p>1</p>
-      </div>
-      <div>
-        <p>1</p>
-      </div>
-      <div>
-        <p>1</p>
-      </div>
-
+      
     </Slider>
+  </div>
   )
 }
 
