1a4b12ec24ce9c677fddfa60959dd3d3b69b80a0
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2022-01-18 12:00:57 +0700

    detail desktop

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,12 +1,9 @@
 import Image from 'next/image'
 import { BsStarFill, BsFillBookmarksFill } from 'react-icons/bs'
-import {MdFavoriteBorder} from 'react-icons/md'
-import {GrFavorite} from 'react-icons/gr'
 import CONFIG from '../../utils/config/config'
 import { fetchDetail } from "../../utils/fetch-ssr"
 import { Gap, Layout, LayoutDetail } from '../../components'
-import { useLocalStorage } from '../../utils/config/useLocalStorage'
-import { useEffect } from 'react/cjs/react.development'
+
 
 
 export async function getServerSideProps({params}) {
@@ -28,43 +25,47 @@ const Detail = ({reqDetail}) => {
   return (
     <LayoutDetail>
       <section>
-        <div className='container-image relative after:content-[""] after:absolute after:top-0 after:bg-black after:w-full after:h-full after:opacity-20'>
+        <div className='container-image relative after:content-[""] after:absolute after:top-0 after:bg-black after:w-full after:h-full lg:after:opacity-70 after:opacity-20'>
           <Image src={CONFIG.BASE_IMAGE_URL + reqDetail.backdrop_path} className='inside-img ' layout='fill'/>
         </div>
-
-        <article className='rounded-3xl bg-gradient-slider-top relative -top-5 '>
-          <div className='p-5 relative '>
-            <div className=' lg:inline-block px-1 pt-1 bg-black hidden'>
-              <Image src={CONFIG.BASE_IMAGE_URL + reqDetail.poster_path} width={200} height={300}/>
+        <article className='rounded-3xl bg-gradient-slider-top b relative -top-5  lg:-top-80  lg:mt-3'>
+          <div className='p-5 relative lg:-top-80 '>
+            <div className='lg:flex gap-10'>
+            <div className='lg:inline-block px-1 pt-1 bg-white hidden '>
+              <Image src={CONFIG.BASE_IMAGE_URL + reqDetail.poster_path} width={400} height={550} />
             </div>
-            <Gap className='h-2' />
-            <p className='text-white font-bold text-2xl'>{reqDetail.title}</p>
-            <Gap className='h-2' />
-          
-            <div className='flex gap-2 items-center'>
-              <div className='flex gap-2 '>
-                {star.map(el => (
-                  <BsStarFill className={star.indexOf(el) > 3 ? `text-white` : `text-red-400`} />
-                ))}
-              </div>
-
-              <p className='text-gray-400 font-bold'>{reqDetail.vote_average}</p>
-            </div>
-            <Gap className='h-2' />
-            <div className='flex gap-4'>
-              {
-                reqDetail.genres.map(genre => (
-                  <p key={genre.id} className='text-gray-700 uppercase text-sm'>{genre.name}</p>
-                ))
-              }
-            </div>
-            <Gap className='h-2' />
+            <div className=''>
+              <Gap className='h-2' />
+              <p className='text-white font-bold text-2xl lg:text-4xl'>{reqDetail.title}</p>
+              <Gap className='h-2' />
             
-            <p className='text-white font-light'>{reqDetail.overview}</p>
-            <Gap className='h-10' />
-            <div className='border p-2 bg-white'>
+              <div className='flex gap-2 items-center'>
+                <div className='flex gap-2 '>
+                  {star.map(el => (
+                    <BsStarFill className={star.indexOf(el) > 3 ? `text-white` : `text-red-400`} />
+                  ))}
+                </div>
+
+                <p className='text-gray-400 font-bold'>{reqDetail.vote_average}</p>
+              </div>
+              <Gap className='h-2' />
+              <div className='flex gap-4'>
+                {
+                  reqDetail.genres.map(genre => (
+                    <p key={genre.id} className='text-gray-700 uppercase text-sm'>{genre.name}</p>
+                  ))
+                }
+              </div>
+              <Gap className='h-2' />
+              
+              <p className='text-white font-light lg:w-10/12'>{reqDetail.overview}</p>
+            </div>
+            </div>
+            <Gap className='h-20' />
+            <div className='border p-2 bg-white lg:w-full lg:rounded-xl'>
               <p className='font-bold'>Production :</p>
-              <Gap className='h-3' />
+              <Gap className='h-3 lg:h-5' />
+              <div className='lg:flex lg:items-center justify-between'>
               {
                 reqDetail.production_companies.map(el => (
                   <div key={el.id} className='mt-3'>
@@ -74,6 +75,7 @@ const Detail = ({reqDetail}) => {
                   </div>
                 ))
               }
+              </div>
             </div>
           </div>
         </article>
