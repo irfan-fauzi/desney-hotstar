a242903f46dde551b9f207605046745df6b593cf
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2022-01-20 15:52:04 +0700

    revenue

--- components/templates/detail-desktop/index.js
+++ components/templates/detail-desktop/index.js
@@ -72,7 +72,21 @@ const DetailDesktop = ({reqDetail}) => {
               <ListActors casts={reqDetail.credits.cast} />
             </div>
             <div className='w-3/12'>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore quia delectus quam expedita assumenda similique quod deleniti iusto quibusdam quasi error, ut fugit asperiores obcaecati nam omnis fuga culpa neque sint molestias deserunt repellendus? Tempore ad temporibus maiores in iure.</p>
+              <p className='font-bold text-xl'>Status :</p>
+              <Gap className='h-2' />
+              <p className='border inline-block border-green-500 px-3 rounded-md py-1'>{reqDetail.status}</p>
+              <Gap className='h-4' />
+              <p className='font-bold text-xl'>Bahasa yang digunakan :</p>
+              <Gap className='h-2' />
+              {reqDetail.spoken_languages.map(lang => (
+                <p key={lang.name} className='italic'>{lang.name}</p>
+              ))}
+               <Gap className='h-4' />
+              <p className='font-bold text-xl'>Anggaran</p>
+              <p>{(reqDetail.budget).toLocaleString('en-US', {style: 'currency',currency: 'USD',})}</p>
+              <Gap className='h-4' />
+              <p className='font-bold text-xl'>Pendapatan</p>
+              <p>{(reqDetail.revenue).toLocaleString('en-US', {style: 'currency',currency: 'USD',})}</p>
             </div>
           </div>
         </div>
