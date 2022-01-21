f9333e9801715adebe5f6ee23c2c63276175334b
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2022-01-21 22:57:10 +0700

    ssr version

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -4,41 +4,33 @@ import { DetailDesktop, DetailMobile, LayoutDetail } from '../../components'
 import { MovieContext, PreviewContext } from "../../utils/config/context"
 import { useRouter } from "next/router"
 
-const Detail = () => {
-  const router = useRouter()
-  const {id} = router.query
+export async function getServerSideProps(context) {
+  const { id } = context.query
+  const detailMovies = await fetchDetail(id)
+  const review = await fetchReviews(id)
+  return {
+    props : {
+      detailMovies,
+      review
+    }
+  }
+}
+
+const Detail = ({detailMovies, review}) => {
   const [detailMovie, setDetailMovie] = useContext(MovieContext)
   const [preview, setPreview] = useContext(PreviewContext)
-  useEffect(() => {
-    let componentMounted = true
-    if(id != null){
-      fetchDetail(id).then((movie) => {
-        if(componentMounted){
-          setDetailMovie(movie)
-        }
-      })
-      fetchReviews(id).then((review) => {
-        if(componentMounted){
-          setPreview(review)
-        }
-      })
-    }
-    return () => { 
-      componentMounted = false
-      setPreview(null)
-      setDetailMovie(null) 
-    }
-  },[id])
-   
+  setDetailMovie(detailMovies)
+  setPreview(review)
+ 
   return (
     <LayoutDetail>
-      {detailMovie && preview && (
+      
           <><div className='lg:hidden block'>
-            <DetailMobile reqDetail={detailMovie} />
+            <DetailMobile/>
           </div><div className='hidden lg:block'>
             <DetailDesktop />
           </div></>
-      )}
+      
     </LayoutDetail>
   )
 }
