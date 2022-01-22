b56e2a134f2641481745f091a2c683c177406c05
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2022-01-22 22:48:47 +0700

    coba lgi

--- components/templates/detail-mobile/index.js
+++ components/templates/detail-mobile/index.js
@@ -1,11 +1,13 @@
-
+import { useContext } from "react"
 import { ReviewComment } from ".."
 import { Gap, InfoTambahan, ListActors, ListCarouselMovie } from "../.."
+import { SimilarMovieContext } from "../../../utils/config/context"
 import ImageHeader from "./image-header"
 import TitleGenreSinopsis from "./title-genre-sinopsis"
 
 const DetailMobile = () => {
-
+  const [similarMovie, setSimilarMovie] = useContext(SimilarMovieContext)
+  
   return (
     <section>
       <article>
@@ -26,7 +28,11 @@ const DetailMobile = () => {
           <p className="text-xl font-semibold">Similar Movies :</p>
         </div>
         <Gap className='h-5' />
-       
+        {
+           similarMovie && (
+             <ListCarouselMovie movies={similarMovie.results} />
+           )
+         }
         <Gap className='h-5' />
       </article>      
   
