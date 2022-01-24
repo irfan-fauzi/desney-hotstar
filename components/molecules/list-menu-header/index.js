24d4c6a41b5dc9f5f1aec635f29e44a5d2e67794
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2022-01-24 16:40:21 +0700

    brasil

--- components/molecules/list-menu-header/index.js
+++ components/molecules/list-menu-header/index.js
@@ -1,29 +1,35 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
+import { useContext } from "react/cjs/react.development"
+import { showMenuContext } from "../../../utils/config/context"
 
 const ListMenuHeader = () => {
+  const router = useRouter()
+  const [showMenu, setShowMenu] = useContext(showMenuContext)
+  
   
   return (
     <ul className='flex gap-10 text-[1rem] lg:flex-row flex-col text-center'>
-      <Link href={`/genre/28`} >
-        <a>
-          <li>Action</li>
-        </a>
-      </Link>
-      <Link href={`/genre/27`}>
-        <a>
-          <li>Horror</li>
-        </a>
-      </Link>
-      <Link href={`/genre/37`}>
-        <a>
-          <li>Western</li>
-        </a>
-      </Link>
-      <Link href={`/genre/99`}>
-        <a>
-          <li>Documentary</li>
-        </a>
-      </Link>
+      
+      <button onClick={() => {
+        router.push('/genre/28')
+        setShowMenu(false)
+      }}>Action</button>
+      
+      <button onClick={() => {
+        router.push('/genre/27')
+        setShowMenu(false)
+      }}>Horror</button>
+      
+      <button onClick={() => {
+        router.push('/genre/37')
+        setShowMenu(false)
+      }}>Western</button>
+     
+      <button onClick={() => {
+        router.push('/genre/99')
+        setShowMenu(false)
+      }}>Documentary</button>
     </ul>
   )
 }
