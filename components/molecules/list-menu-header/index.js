4b4eb3f36be9f700a2cf1694c000c9fadda7854a
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2022-01-24 18:05:59 +0700

    clear header

--- components/molecules/list-menu-header/index.js
+++ components/molecules/list-menu-header/index.js
@@ -18,22 +18,44 @@ const MyButton = ({title, href}) => {
 
 const ListMenuHeader = () => {
   
-  
   return (
-    <ul className='flex gap-10 text-[1rem] lg:flex-row flex-col text-center'>
-      <Link href={`/genre/28`} >
-        <MyButton title={'Action'} href={'/genre/28'} />        
+    <><ul className='flex gap-10 text-[1rem] lg:hidden lg:flex-row flex-col text-center '>
+      <Link href={`/genre/28`}>
+        <MyButton title={'Action'} href={'/genre/28'} />
       </Link>
       <Link href={`/genre/27`}>
-        <MyButton title={'Horror'} href={`/genre/27`}/>
+        <MyButton title={'Horror'} href={`/genre/27`} />
       </Link>
       <Link href={`/genre/37`}>
         <MyButton title={'Western'} href={`/genre/37`} />
       </Link>
       <Link href={`/genre/99`}>
-        <MyButton title={'Documentary'} href={`/genre/99`}/>
+        <MyButton title={'Documentary'} href={`/genre/99`} />
       </Link>
     </ul>
+    
+    <ul className='hidden lg:flex gap-10 text-[1rem] lg:flex-row flex-col text-center'>
+        <Link href={`/genre/28`}>
+          <a>
+            <li>Action</li>
+          </a>
+        </Link>
+        <Link href={`/genre/27`}>
+          <a>
+            <li>Horror</li>
+          </a>
+        </Link>
+        <Link href={`/genre/37`}>
+          <a>
+            <li>Western</li>
+          </a>
+        </Link>
+        <Link href={`/genre/99`}>
+          <a>
+            <li>Documentary</li>
+          </a>
+        </Link>
+      </ul></>
   )
 }
 
