1174861718567300e790b911f188c73ecb8ce69b
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2022-01-24 17:50:45 +0700

    cler

--- components/molecules/list-menu-header/index.js
+++ components/molecules/list-menu-header/index.js
@@ -1,35 +1,38 @@
 import Link from "next/link"
-import { useRouter } from "next/router"
-import { useContext } from "react/cjs/react.development"
+import { useContext } from "react"
 import { showMenuContext } from "../../../utils/config/context"
 
-const ListMenuHeader = () => {
-  const router = useRouter()
+
+const MyButton = ({title, href}) => {
   const [showMenu, setShowMenu] = useContext(showMenuContext)
+  return(
+    <a href={href} className="cursor-pointer" onClick={() => {
+      setShowMenu(false)
+      
+      }
+    }
+    >{title}</a>
+  )
+  
+}
+
+const ListMenuHeader = () => {
   
   
   return (
     <ul className='flex gap-10 text-[1rem] lg:flex-row flex-col text-center'>
-      
-      <button onClick={() => {
-        router.push('/genre/28')
-        setShowMenu(false)
-      }}>Action</button>
-      
-      <button onClick={() => {
-        router.push('/genre/27')
-        setShowMenu(false)
-      }}>Horror</button>
-      
-      <button onClick={() => {
-        router.push('/genre/37')
-        setShowMenu(false)
-      }}>Western</button>
-     
-      <button onClick={() => {
-        router.push('/genre/99')
-        setShowMenu(false)
-      }}>Documentary</button>
+      <Link href={`/genre/28`} >
+        <MyButton title={'Action'} href={'/genre/28'} />        
+      </Link>
+      <Link href={`/genre/27`}>
+        <MyButton title={'Horror'} href={`/genre/27`}/>
+      </Link>
+      <Link href={`/genre/37`}>
+        <MyButton title={'Western'} href={`/genre/37`} />
+      </Link>
+      <Link href={`/genre/99`}>
+        <MyButton title={'Documentary'} href={`/genre/99`}/>
+      </Link>
     </ul>
   )
 }
--- pages/genre/[id].js
+++ pages/genre/[id].js
@@ -1,5 +1,7 @@
+import { useEffect, useContext } from 'react';
 import { Gap, LayoutDetail, MenuButtonMobile, MoviesThumbnail } from '../../components';
 import CONFIG from '../../utils/config/config';
+import { showMenuContext } from '../../utils/config/context';
 import { fetchGenre } from '../../utils/fetch-ssr';
 
 
@@ -23,16 +25,16 @@ export const getStaticProps = async (context) => {
   const genres = await fetchGenre(id)
   return {
     props: {
-      genres
+      genres,id
     }
   }
 }
 
 
-const GenresMovies = ({genres}) => {
-
+const GenresMovies = ({genres, id}) => {
+ 
   const movies = genres.results
-  
+ 
   return (
     <LayoutDetail>
       <MenuButtonMobile />
