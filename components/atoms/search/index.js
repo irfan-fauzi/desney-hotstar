ad48f7f4ed319a9d7d2d8075d7fd6586cc05976b
irfan-fauzi@yahoo.com <irfanwintech@yahoo.com>
2022-01-25 14:14:09 +0700

    add search fiture

--- components/atoms/search/index.js
+++ components/atoms/search/index.js
@@ -1,16 +1,53 @@
+import Image from 'next/image'
+import axios from 'axios';
+import { useEffect, useState } from 'react'
 import { BiSearch } from 'react-icons/bi'
+import API_ENDPOINT from '../../../utils/config/api-endpoint';
+import CONFIG from '../../../utils/config/config';
 
 const Search = () => {
+  const [loading, setLoading] = useState(false)
+  const [value, steValue] = useState('')
+  const [movies, setMovies] = useState(null)
+
+  const searchMovie = async (value) => {
+    if(value == ""){
+      return false 
+    }
+    setLoading(true)
+    const response = await axios(API_ENDPOINT.SEARCH_MOVIE(value))
+    const moviesResponse = await response.data.results
+    setMovies(moviesResponse)
+    setLoading(false)
+  }
+
+  const onChangeHandler = async (e) => {
+    searchMovie(e.target.value)
+    steValue(e.target.value)
+  }
+
   return (
-    <>
-    <input 
-      type="text" 
-      className='bg-transparent outline-none ' 
-      placeholder='Search' />
-         <button>
-           <BiSearch className='text-3xl lg:text-2xl'/>
-        </button>
-    </>
+    <div className='relative  flex items-center w-full justify-between'>
+      <input 
+        onChange={onChangeHandler}
+        value={value}
+        type="text" 
+        className='bg-transparent outline-none ' 
+        placeholder='Search'
+      />
+      <button>
+        <BiSearch className='text-3xl lg:text-2xl'/>
+      </button>
+      <article className='absolute w-full top-10 left-0 bg-black bg-opacity-20 overflow-auto z-50'>
+        {
+          value && movies && movies.map((movie) => (
+            <div key={movie.id}>
+              <p>{movie.title}</p>
+            </div>
+          ))
+        }
+      </article>  
+    </div>
   )
 };
 
