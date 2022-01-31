92adb749ee743b606f670aee8352299dc46cd6b5
Irfan fauzi Rahmatullah <56503242+irfan-fauzi@users.noreply.github.com>
2022-01-31 10:43:32 +0700

    Update index.js

--- components/organisms/footer/index.js
+++ components/organisms/footer/index.js
@@ -8,14 +8,16 @@ const Footer = () => {
     <footer className='bg-black px-10 py-7 text-white mt-auto'>
       <div className='flex items-center justify-between lg:flex-row flex-col lg:max-w-screen-xl lg:mx-auto'>
         <div className='lg:w-4/12 lg:block flex flex-col'>
-          <img src="/assets/images/logo.png" alt='logo' className='w-[150px]' />
+          <div className="flex item center justify-center">
+            <img src="/assets/images/logo.png" alt='logo' className='w-[150px]' />
+          </div>
           <Gap className='h-5 lg:hidden' />
           <p className='text-sm lg:text-left text-center'>©2022 Movie planet dan entitas-entitas terkaitnya. Hak Cipta Dilindungi Undang-undang. © 2022 Entitas-entitas pemberi lisensi, Hak Cipta Dilindungi Undang-undang </p>
         </div>
         <Gap className='h-5 lg:hidden' />
         <div className='flex items-start  lg:items-center lg:flex-row flex-col gap-5'>
           <div>
-           <p className='text-lg text-center lg:text-left font-semibold'>Desney Hotstar App</p>
+           <p className='text-lg text-center lg:text-left font-semibold'>Movie Planet App</p>
           <p>dapat diunduh secara gratis di :</p>
           </div>
           <Image src={appStore} height={140} width={200} />
